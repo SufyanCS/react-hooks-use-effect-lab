c610c471421dd8ce3a79e08dd5d85c59d9106f57
Sufyan <sufianb.contact@gmail.com>
2023-03-25 17:25:37 +0300

    Completed assignment 'the code works as required but didn't pass the tests'

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,8 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 function Question({ question, onAnswered }) {
   const [timeRemaining, setTimeRemaining] = useState(10);
-
+useEffect (() => {
+  if (timeRemaining > 0) {
+    const timer = setTimeout(() => {
+      setTimeRemaining ((timeRemaining) => timeRemaining -1)
+    }, 1000);
+    return () => clearTimeout(timer);
+  }
+  else {
+    setTimeRemaining(10);
+    onAnswered(false);
+  }
+  
+})
   // add useEffect code
 
   function handleAnswer(isCorrect) {
